157763668437e56ff25493f2ad6db306b2a37c9d
Oslo <18248392627@163.com>
2018-07-31 10:00:27 +0800

    add title

--- er_db/admin.py
+++ er_db/admin.py
@@ -13,6 +13,6 @@ class The_UserAdmin(admin.ModelAdmin):
     #list_filter = ('is_custom',)
     #list_editable = ['name', 'tel_no']
 
-admin.site.site_header = 'qgsowin.com'
-admin.site.site_title = 'Oslo'
+admin.site.site_header = '国家职业资格证书管理系统'
+admin.site.site_title = '国家职业资格证书管理系统'
 admin.site.register(Mob_User,The_UserAdmin)
